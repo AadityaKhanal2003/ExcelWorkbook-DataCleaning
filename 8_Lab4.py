db3d2f704dd9b92570cfc00acd18ff604f471662
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2023-09-26 14:46:38 -0400

    Update 8_Lab4.py

--- 8_Lab4.py
+++ 8_Lab4.py
@@ -2,6 +2,10 @@ import openpyxl
 import xlrd
 
 path = "Lab4Data.xlsx"
-wb = openpyxl.load_workbook(path)
+wb = openpyxl.load_workbook(path, read_only=False)
+worksheet_names = wb.sheetnames
+print(worksheet_names)
+sheet_index = worksheet_names.index('Table 9 ')
+wb.active = sheet_index
 ws = wb.active
 print(ws)
