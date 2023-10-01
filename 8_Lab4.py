b8c29083db37428e9c3e2c9a2f733bbd76890207
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2023-09-30 21:26:03 -0400

    Update 8_Lab4.py

--- 8_Lab4.py
+++ 8_Lab4.py
@@ -1,5 +1,5 @@
 import openpyxl
-
+import xlrd
 path = "Lab4Data.xlsx"
 wb = openpyxl.load_workbook(path, read_only=False)
 ws = wb.worksheets[1]
@@ -12,26 +12,25 @@ def calctotalmarriage():
     total_marriage_value = []
     cmarraigeb15_range= ws["K15:K211"]
     cmarraigeb18_range= ws["M15:M211"]
-    cmarraige_range = ws["K15:L211"]
 
     # Iterate through child marriage values
-    for i in range(0, len(cmarraige_range)):
+    for i in range(0, len(cmarraigeb15_range)):
         cmarraigeb15_value = cmarraigeb15_range[i][0].value
         cmarraigeb18_value= cmarraigeb18_range[i][0].value
         if("–" in str(cmarraigeb15_value) or "–" in str(cmarraigeb18_value)):
             total_marriage_value.append("–")
         else:
-            cmarraigeb15_value = round(float(cmarraigeb15_value), 1)
-            cmarraigeb18_value = round(float(cmarraigeb18_value), 1)
-            toaddd = int(cmarraigeb15_value + cmarraigeb18_value)
-            total_marriage_value.append(toaddd)
+            cmarraigeb15_value = float(cmarraigeb15_value)
+            cmarraigeb18_value = float(cmarraigeb18_value)
+            toaddd = (cmarraigeb15_value + cmarraigeb18_value)/2
+            to_add_roundedd = round(toaddd,1)
+            total_marriage_value.append(to_add_roundedd)
     return total_marriage_value
 
 def calctotaljow():
     total_jow_output = []
     total_jow_rangeM= ws["W15:W211"]
     total_jow_rangeF= ws["Y15:Y211"]
-    total_jow  = ws["W15:Y211"]
 
     # Iterate through Justification of Wife Beating
     for i in range(0, len(total_jow_rangeM)):
@@ -40,19 +39,46 @@ def calctotaljow():
         if "–" in str(jowM_value) or "–" in str(jowF_value):
             total_jow_output.append("–")
         else:
-            jowM_value = round(float(jowM_value), 1)
-            jowF_value = round(float(jowF_value), 1)
-            toadd = int(jowM_value + jowF_value)
-            total_jow_output.append(toadd)
+            jowM_value = float(jowM_value)
+            jowF_value = float(jowF_value)
+            to_add = (jowM_value + jowF_value)/2
+            to_add_roundedd = round(to_add, 1)
+            total_jow_output.append(to_add_roundedd)
     return total_jow_output
 
+def calctotalfgm():
+    total_fgm_output = []
+    total_fgm__rangeW= ws["Q15:Q211"]
+    total_fgm__rangeG= ws["S15:S211"]
+    total_fgm__rangeSFTP= ws["U15:U211"]
+
+    # Iterate through Justification of Female genital cutting
+    for i in range(0, len(total_fgm__rangeW)):
+        fgmW_value = total_fgm__rangeW[i][0].value
+        fgmG_value= total_fgm__rangeG[i][0].value
+        fgmSFTP_value= total_fgm__rangeSFTP[i][0].value
+        if "–" in str(fgmW_value) or "–" in str(fgmG_value) or "–" in str(fgmSFTP_value):
+            total_fgm_output.append("–")
+        else:
+            fgmW_value = float(fgmW_value)
+            fgmG_value = float(fgmG_value)
+            fgmSFTP_value = float(fgmSFTP_value)
+            toadd = (fgmW_value + fgmG_value+ fgmSFTP_value)/3
+            to_add_rounded = round(toadd, 1)
+            total_fgm_output.append(to_add_rounded)
+    return total_fgm_output
+
+
+
+#7columns including country_values
 country_values = [cell[0].value for cell in countries_range] #197
 clabour_values = [cell[0].value for cell in clabour_range] #197
 bregistration_values = [cell[0].value for cell in bregistration_range] #197
 vdiscpline_values = [cell[0].value for cell in vdiscpline_range]#197
-total_marriage_value = calctotalmarriage()
-total_wife_beating_value = calctotaljow()
-print(total_wife_beating_value)
+total_marriage_value = calctotalmarriage() #197
+total_wife_beating_value = calctotaljow()#197
+total_female_genital_Value = calctotalfgm()#197
+print(total_marriage_value)
 wb.close()
 
 
