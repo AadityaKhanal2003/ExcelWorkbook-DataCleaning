170c618805905327e18c4e00ee1910d739c493bc
Gaurab-B <54475579+Gaurab-B@users.noreply.github.com>
2023-10-06 09:37:58 -0400

    Update 8_Lab5.py

--- 8_Lab5.py
+++ 8_Lab5.py
@@ -1,39 +1,30 @@
+#Gaurab Baral, Aditya Khanal, Lab 5 , Group 8
 import slate3k
 
 filename = "Table9.pdf"
-n = 14
+n = 14 #gap between two countries and their data
 outer_list = []
+
 
 with open(filename, 'rb') as fpointer:
     pdf = slate3k.PDF(fpointer)
-    
     temp_list = []
     for content in pdf:
-        # Remove unwanted line breaks and spaces
-        content = content.strip()
-        
-        # Skip empty lines
-        if not content:
+        content = content.strip()     # Remove unwanted line breaks and spaces
+        if not content:# Skip empty lines
             continue
-
-        temp_list.append(content)
-
-        if len(temp_list) == n:
-            outer_list.append(temp_list)
-            temp_list = []
-
-    # Append any remaining content as the last sublist
-    if temp_list:
-        outer_list.append(temp_list)
+        outer_list.append(content) #add from pdf_file to temp_list
     fpointer.close()
+
+
 lines = []
+
 for sublist in outer_list:
-    sublist_lines = []
-    for item in sublist:
-        item_lines = item.split('\n')[12:]
-        item_lines = [line for line in item_lines if line]
-        sublist_lines.extend(item_lines)
-    lines.append(sublist_lines)
+    item_lines = sublist.split('\n')[12:] #split the data based on "\n" and start from 12 to remove TABLE9 in the output.
+    item_lines = [line for line in item_lines if line] #a list called item_lines and filter out any empty line
+    lines.append(item_lines) #add item_lines to lines list
+
+
 temp_list2 = []
 outer_list2 = []
 for data in lines:
@@ -43,17 +34,20 @@ for data in lines:
         if skip_next > 0:
             skip_next -= 1
             continue  # Skip the next 41 items after "CHILD PROTECTION"
-
         if "CHILD PROTECTION" in each_item:
             skip_next = 36
-            continue  # Skip the "CHILD PROTECTION" item itself
+            continue  
         if "SUMMARY INDICATORS" in each_item:
             break
         temp_list2.append(each_item)
-
         if (index + 1) % 15 == 0:
             outer_list2.append(temp_list2)
             temp_list2 = []
 
+
+#Display the output
 for item in outer_list2:
     print(item)
+
+
+
